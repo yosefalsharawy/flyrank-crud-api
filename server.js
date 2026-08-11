fb54ff09cb39c96c2a27bb4ed18b2cd2adcb98ca
yosefalsharawy <yousef150405@eng.psu.edu.eg>
2026-08-11 19:49:41 +0300

    Stage 4: complete CRUD API

--- server.js
+++ server.js
@@ -71,6 +71,42 @@ app.post('/tasks', (req, res) => {
 	res.status(201).json(newTask);
 });
 
+app.put('/tasks/:id', (req, res) => {
+	const { id } = req.params;
+	const { title, done } = req.body;
+
+	const hasTitle = req.body.hasOwnProperty('title');
+	const hasDone = req.body.hasOwnProperty('done');
+
+	if (!hasTitle && !hasDone) {
+		return res.status(400).json({ error: `title or done are required` });
+	}
+	if (hasTitle && (typeof title !== 'string' || title.trim() === '')) {
+		return res.status(400).json({ error: `Invalid title` });
+	}
+	if (hasDone && typeof done !== 'boolean') {
+		return res.status(400).json({ error: `done is invalid` });
+	}
+	const task = tasks.find((elem) => elem.id.toString() === id);
+	if (!task) {
+		return res.status(404).json({ error: `task is not found` });
+	}
+	if (hasTitle) task.title = title.trim();
+	if (hasDone) task.done = done;
+
+	res.status(200).json(task);
+});
+
+app.delete('/tasks/:id', (req, res) => {
+	const { id } = req.params;
+	const taskIndex = tasks.findIndex((elem) => elem.id.toString() === id);
+	if (taskIndex === -1) {
+		return res.status(404).json({ error: `task is not found` });
+	}
+	tasks.splice(taskIndex, 1);
+	res.sendStatus(204);
+});
+
 app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
